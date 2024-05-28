1989e778113821f3d83afe37da586a0cf6fc0a49
Santiago <santiagocanon2390@gmail.com>
2024-05-27 19:32:39 -0500

    Limpiar

--- Pagina web - proyecto/codeJavaS.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".logo").hover(function(){
-        $(this).css("background-color","#575757").css("borderRadius","4px")
-    }, function(){
-        $(this).css("background-color","").css("borderRadius","")
-    })
-})
